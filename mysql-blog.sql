800b4a4ec02f55e009acba8eee8d4a7bde6fd5c3
coq-j01 <chaebinesxo@gmail.com>
2025-03-27 09:40:31 +0900

    Update mysql-blog.sql

--- mysql-blog.sql
+++ mysql-blog.sql
@@ -1,14 +1,20 @@
 -- mysql blog
-use mydb;
 
+-- 데이터베이스 생성
+CREATE DATABASE IF NOT EXISTS blog;
+
+-- 데이터베이스 선택
+USE blog;
 create table posts( 
 	id int primary key auto_increment,
 	title varchar(50) not null,
-	content varchar(100) not null
+	content varchar(200) not null
 );
 create table comments(
-	id int primary key auto_increment,
+	comment_id int primary key auto_increment,
 	post_id int,
-	content varchar(100) not null,
+	comment_content varchar(200) not null,
 	foreign key(post_id) references posts(id) on delete cascade
 );
+
+select * from comments;
